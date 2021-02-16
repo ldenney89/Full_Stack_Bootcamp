ccb05b0063dcc761c49e4e6129450c2022f2b098
Laura Denney <laura.a.denney@gmail.com>
2021-02-16 07:04:01 -0800

    work on myfun()

--- day2/static/welcome.js
+++ day2/static/welcome.js
@@ -4,4 +4,50 @@ function validate() {
         document.myform.name.focus()
         return false
     }
+    if(document.myform.email.value ==""){
+        alert('enter your email')
+        document.myform.email.focus()
+        return false
+    }
+    if(document.myform.zipcode.value =="" || isNaN(document.myform.zipcode.value) || document.myform.zipcode.value.length !=5){
+        alert('enter your zipcode')
+        document.myform.zipcode.focus()
+        return false
+    }
+    if(document.myform.country.value =="-1"){
+        alert('enter your country')
+        document.myform.country.focus()
+        return false
+    }
+}
+function mouseactive() {
+    alert('mouse entered')
+}
+function mouseleft() {
+    alert('mouse left')
+}
+
+function myfun3(){
+    var txt
+    var person =prompt('enter name')
+
+    if(person ==null || person=="") {
+        txt= 'you cancelled'
+    }
+    else {
+        txt = 'hello ' + person
+    }
+    document.getElementById('pro').innerHTML=txt
+}
+
+function myfun4() {
+    var usergrades='a'
+
+    switch(usergrades) {
+        case 'a' : document.write('you have good grades')
+        break;
+        case 'b' : document.write('you have nice grades')
+        break;
+        default : document.write('grade not found')
+    }
 }
